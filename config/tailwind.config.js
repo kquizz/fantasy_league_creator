cc0ab4ee7f9a97668d2f367e65f7e7c094869459
Kquizz <quillen.kevin@gmail.com>
2023-11-20 02:54:03 -0600

    Adds Tailwind + Basic Styles

---
+++ config/tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+ content: ["./app/**/*.{html,js,erb}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
