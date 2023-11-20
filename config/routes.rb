b8fa6fade70d24115c304d46b1753a4742f1126e
Kquizz <quillen.kevin@gmail.com>
2023-11-19 20:44:28 -0600

    adds User Management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  root :to => 'users#index'
+  resources :users
+
+get 'login' => 'user_sessions#new', :as => :login
+post 'login' => "user_sessions#create"
+get 'logout' => 'user_sessions#destroy', :as => :logout
+post 'logout' => 'user_sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
