dae1a30cf7b5f544e9dd1db4d25d02a0bd1f8857
Kevin Quillen <quillen.kevin@gmail.com>
2023-11-27 14:44:58 -0600

    Adds Virtual attributes to Event Adds Styling all over

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,39 @@
 class Event < ApplicationRecord
+  attr_accessor :has_begun, :has_ended, :time_until_string, :full_title, :season_length
+
   has_one_attached :banner
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
+
+  def has_began
+    season_start > Date.today ? true : false
+  end
+
+  def has_ended
+    season_start > Date.today ? true : false
+  end
+
+  def full_title
+    "#{title} - Season: #{season}"
+  end
+
+  def season_length
+     (season_end - season_start).ceil
+  end 
+
+  def time_until_string
+    case season_start
+    when Date.today
+      "Starts today!"
+    when Date.tomorrow
+      "Starts tomorrow!"
+    when Date.today..Date.tomorrow
+      "Starts within the next two days!"
+    when Date.today..(Date.today + 6)
+      "Starts within the next week!"
+    when Date.new(1899, 1, 1)..Date.yesterday
+      "Already started!"
+    else
+      "Starts in #{(season_start - Date.today).ceil} days"
+    end
+  end
 end
